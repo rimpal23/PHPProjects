7ce9259f34d7b847a9a5df91107c8e182461ced0
rimpaldhillon <rimpaldhillon@192.168.0.199>
2023-02-02 11:53:58 -0500

    Minor Change

--- AdrressBook/src/AddressBook/AddressController.java
+++ AdrressBook/src/AddressBook/AddressController.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AddressBook;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 
-/**
- *
- * @author stest
- */
 public class AddressController {
 
 	private ArrayList<AddressModel> addressBook;
--- AdrressBook/src/AddressBook/AddressView.java
+++ AdrressBook/src/AddressBook/AddressView.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AddressBook;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author 
- */
 public class AddressView {
 
 	private AddressController controller;
